a1195727f3b99c0e07ac53771397a1055f229261
MINHO KIM <minhok913@gmail.com>
2024-06-20 06:37:36 +0000

    change

---
+++ eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "es2021": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 12
+  },
+  "rules": {
+    // 여기에 ESLint 규칙을 추가 또는 수정하세요
+  }
+};
